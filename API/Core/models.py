d45c1b04b1681b7e00c2350feda693a12ba20a2d
nishu-02 <0264nishu@gmail.com>
2025-04-17 19:54:19 +0530

    Adding the pillow for the images and adding more the to the models

--- API/Core/models.py
+++ API/Core/models.py
@@ -4,5 +4,13 @@ from django.db import models
 class Product(models.Model): # Product is the name of the table
     product_id = models.AutoField(primary_key=True) # AutoField is an auto-incrementing integer field
     product_name = models.CharField(max_length=60)
+    category = models.CharField(max_length=30)
+    subcategory = models.CharField(max_length=40)
+    price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300, default="description")
     pub_date = models.DateField()
+    image = models.ImageField(upload_to='Core/images', default="") # upload_to is the path where the image will be stored   
+
+    def __str__(self):
+        return self.product_name # so when the whole list is displayed here we will choose what to show in the list (previously it was showing the object name)
+     
