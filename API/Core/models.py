4c1eb076c921c3353b0e6b1e21d8fa03bce97460
nishu-02 <0264nishu@gmail.com>
2025-04-17 19:33:39 +0530

    Adding and registering the models

--- API/Core/models.py
+++ API/Core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model): # Product is the name of the table
+    product_id = models.AutoField(primary_key=True) # AutoField is an auto-incrementing integer field
+    product_name = models.CharField(max_length=60)
+    desc = models.CharField(max_length=300, default="description")
+    pub_date = models.DateField()
