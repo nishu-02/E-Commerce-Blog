7516de687471cf5569adb4737e50f48400c52ff5
nishu-02 <0264nishu@gmail.com>
2025-04-25 23:00:09 +0530

    views for the getting blog

--- API/Blog/views.py
+++ API/Blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blogpost
 
 # create your views here.
 
 def index(request):
-    return render(request, 'Blog/index.html')
+    myposts = Blogpost.objects.all()
+    print(myposts)
+    return render(request, 'Blog/index.html', {'myposts': myposts})
+
+def blogpost(request, id):
+    post = Blogpost.objects.filter(post_id=id)[0]
+    print(post)
+    return render(request, 'Blog/blogpost.html', {'post': post})
